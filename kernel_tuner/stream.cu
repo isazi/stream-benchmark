850a68ef13eadd958ced00af58dff7ca5986b8a0
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-11-13 22:07:55 -0700

    First CUDA implementation.

---
+++ kernel_tuner/stream.cu
@@ -0,0 +1,38 @@
+
+__global__ void copy(const TYPE* a, TYPE* c, const int size) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if ( i < size ) {
+        c[i] = a[i];
+    }
+}
+
+__global__ void scale(const TYPE scalar, TYPE* b, const TYPE* c, const int size) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if ( i < size ) {
+        b[i] = scalar * c[i];
+    }
+}
+
+__global__ void add(const TYPE* a, const TYPE* b, TYPE* c, const int size) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if ( i < size ) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+__global__ void triad(const TYPE scalar, TYPE* a, const TYPE* b, const TYPE* c, const int size) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if ( i < size ) {
+        a[i] = b[i] + scalar * c[i];
+    }
+}
+
+__global__ void stream(const TYPE scalar, TYPE* a, TYPE* b, TYPE* c, const int size) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if ( i < size ) {
+        c[i] = a[i];
+        b[i] = scalar * c[i];
+        c[i] = a[i] + b[i];
+        a[i] = b[i] + scalar * c[i];
+    }
+}
