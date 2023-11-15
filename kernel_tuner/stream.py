d025627af6cd082255491554b4ed9ed5f0c54db1
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2023-11-15 16:54:17 -0700

    Memory bandwidth.

--- kernel_tuner/stream.py
+++ kernel_tuner/stream.py
@@ -14,7 +14,7 @@ def parse_cli():
     return parser.parse_args()
 
 
-def tune_copy(size: int, type: str):
+def tune_copy(size: int, type: str, elem_size: int):
     with open("stream.cu", "r") as file:
         source = file.read()
 
@@ -29,15 +29,30 @@ def tune_copy(size: int, type: str):
     tune_params["block_size_x"] = [32 * i for i in range(1, 33)]
     tune_params["TYPE"] = [type]
 
-    tune_kernel("copy", source, size, args, tune_params, answer=answer, lang="cupy")
+    metrics = dict()
+    metrics["GB/s"] = lambda p: (2 * elem_size * size / 10**9) / (p["time"] / 10**3)
+
+    tune_kernel(
+        "copy",
+        source,
+        size,
+        args,
+        tune_params,
+        answer=answer,
+        lang="cupy",
+        metrics=metrics,
+    )
 
 
 arguments = parse_cli()
 if arguments.float:
     type = "float"
+    elem_size = 4
 elif arguments.half:
     type = "half"
+    elem_size = 2
 else:
     type = "double"
+    elem_size = 8
 print("Tuning copy")
-tune_copy(arguments.size, type)
+tune_copy(arguments.size, type, elem_size)
